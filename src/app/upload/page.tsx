5877cc2914fc4343e3ad8fab0b08f418d18314ed
Hiekki <hiekkii@gmail.com>
2026-02-22 22:07:17 -0600

    pushed uploads down

--- src/app/upload/page.tsx
+++ src/app/upload/page.tsx
@@ -22,7 +22,7 @@ export default function Upload() {
     }
 
     return (
-        <div className='p-20'>
+        <div className='p-20 mt-48'>
             <input type='file' onChange={(e) => setFile(e.target.files?.[0] || null)} />
             <button onClick={upload} className='ml-4 px-4 py-2 bg-sage text-white rounded'>
                 Upload
