f9521c527c232aef0c32cbad2cd8a4c86abad980
Hiekki <hiekkii@gmail.com>
2026-02-22 22:04:53 -0600

    changed photos

--- src/components/Gallery.tsx
+++ src/components/Gallery.tsx
@@ -1,29 +1,30 @@
-import Image from 'next/image';
-import fs from 'fs';
-import path from 'path';
+// import Image from 'next/image';
+// import fs from 'fs';
+// import path from 'path';
 
 export default function Gallery() {
-    const photosDirectory = path.join(process.cwd(), 'public/gallery');
-    const files = fs.readdirSync(photosDirectory);
+    return <div className='flex min-h-screen items-center justify-center font-alex text-8xl'>Photos</div>;
+    // const photosDirectory = path.join(process.cwd(), 'public/gallery');
+    // const files = fs.readdirSync(photosDirectory);
 
-    const photos = files.filter((file) => /\.(jpg|jpeg|png|webp)$/i.test(file)).map((file) => `/gallery/${file}`);
+    // const photos = files.filter((file) => /\.(jpg|jpeg|png|webp)$/i.test(file)).map((file) => `/gallery/${file}`);
 
-    return (
-        <section className='mx-auto max-w-6xl px-4 py-10'>
-            <div className='columns-2 gap-4 lg:columns-4'>
-                {photos.map((src) => (
-                    <div key={src} className='mb-4 break-inside-avoid'>
-                        <Image
-                            src={src}
-                            alt=''
-                            width={1000}
-                            height={1000}
-                            sizes='(max-width: 1024px) 50vw, 25vw'
-                            className='w-full h-auto rounded-2xl'
-                        />
-                    </div>
-                ))}
-            </div>
-        </section>
-    );
+    // return (
+    //     <section className='mx-auto max-w-6xl px-4 py-10'>
+    //         <div className='columns-2 gap-4 lg:columns-4'>
+    //             {photos.map((src) => (
+    //                 <div key={src} className='mb-4 break-inside-avoid'>
+    //                     <Image
+    //                         src={src}
+    //                         alt=''
+    //                         width={1000}
+    //                         height={1000}
+    //                         sizes='(max-width: 1024px) 50vw, 25vw'
+    //                         className='w-full h-auto rounded-2xl'
+    //                     />
+    //                 </div>
+    //             ))}
+    //         </div>
+    //     </section>
+    // );
 }
