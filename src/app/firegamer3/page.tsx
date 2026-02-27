6b9dc752d5ed37ef7fa8dae0838240d9b40cf569
Hiekki <hiekkii@gmail.com>
2026-02-26 20:57:37 -0600

    lol

---
+++ src/app/firegamer3/page.tsx
@@ -0,0 +1,14 @@
+import { Metadata } from 'next';
+import BorderFrame from '@/components/BorderFrame';
+
+export const metadata: Metadata = {
+    title: 'Haley and Kevin | FireGamer3',
+};
+
+export default function FAQsPage() {
+    return (
+        <div className='flex items-center text-center justify-center min-h-[calc(100vh-6rem)] lg:min-h-[calc(100vh-9rem)] w-full font-alex text-7xl'>
+            {`o hai 🙃`}
+        </div>
+    );
+}
